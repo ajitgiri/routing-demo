761d0aeab3c3565fc77fe7af742fc5a375fe8301
KumarAjit <ajit.sipu@gmail.com>
2021-09-08 04:59:00 +0530

    initial push

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeDetailsComponent } from './componnents/home-details/home-details.component';
+import { HomeListComponent } from './componnents/home-list/home-list.component';
+import { HomeComponent } from './componnents/home/home.component';
+import { LoginComponent } from './componnents/login/login.component';
 
+const routes: Routes = [
+  {path:"", component:LoginComponent},
+  {path:"home", component:HomeComponent,
+    children: [
+                {path:"homeDetails", component:HomeDetailsComponent},
+                {path:"homeList", component:HomeListComponent},
 
-const routes: Routes = [];
+              ]
+  },
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './componnents/login/login.component';
+import { HomeComponent } from './componnents/home/home.component';
+import { HomeDetailsComponent } from './componnents/home-details/home-details.component';
+import { HomeListComponent } from './componnents/home-list/home-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    HomeDetailsComponent,
+    HomeListComponent
   ],
   imports: [
     BrowserModule,
